f4c187bc825bb0236c569dc1c9a5a4e909a501f1
John Michael Robey <robey14@gmail.com>
2020-01-30 01:45:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+
+def reverse_each_word(sentence)
+  reversed = (sentence.split.collect do |word|
+    word.reverse
+  end).join(' ')
+end
+
+puts reverse_each_word("This is a sentence to reverse the words of")
+
+# Misread the assignment and thought it was this
+def reverse_word_order(sentence)
+  reversed = []
+  sentence.split.each do |word|
+    reversed.unshift word
+  end
+  reversed.join(' ')
+end
+
+puts reverse_word_order("This is a sentence to reverse the word order of")
